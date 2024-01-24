f0110e6e0265dadff5fb9e0339c1b0e851c04a6e
yohanscar <wyohan@gmail.com>
2024-01-24 12:36:11 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-aws-cloud-gaming-v2"
-    key    = "terraform-state-aws-cloud-gaming-v2.tfstate"
+    bucket = "terraform-state-aws-cloud-gaming-v99"
+    key    = "terraform-state-aws-cloud-gaming-v99.tfstate"
     region = "sa-east-1"
   }
 }
